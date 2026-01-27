3f98d1d52f110fe8fcfa33cba39b59d6cd18abed
Rahul Modugu <grover@MacBookPro.lan>
2026-01-27 06:24:42 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "rahul"}
+
+@app.get('/first')
+def first():
+    return {'this is fast api : mix of first server' : 'wellcome'}
+
+
+@app.get('/second')
+def second():
+    return {'message':'this is second page'}
+
+@app.get('/second/{name}')
+def first_name(name:str):
+    return {'message':'grover'}
+
+
+
+@app.get("/users")
+def get_users():
+    return ["user1", "user2"]
+
+
+
+@app.get("/users/{user_id}")
+def krishna(user_id: int):
+    return {"user_id": user_id}
+
+@app.get('/third')
+def krishna():
+    return {'this is third page':'welcome'}
+
+@app.get('/third/{name}')
+def krisna(name : str, age : int):
+    return {f'welcome to india this is {name}': f'and i am {age} old'}
